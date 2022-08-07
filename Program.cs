c11ca5bdecfc87aeee70ab9f713b783904b7cf1e
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-07 14:32:32 +0300

    Lesson1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// // See https://aka.ms/new-console-template for more information
+//  Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.WriteLine("Введите Оператор");
+    string Operator = Console.ReadLine();
+    if (Operator == "end")
+    {
+        break;
+    }
+    Console.WriteLine("Введите первое число");
+
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+
+    int number2 = Convert.ToInt32(Console.ReadLine());   
+     
+    if (Operator == "+")
+    {
+        Console.WriteLine("Мы сложили числа");
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Operator == "-")
+    {
+        Console.WriteLine("Мы вычли числа");
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Operator == "*")
+    {
+        Console.WriteLine("Мы умножили числа");
+        Console.WriteLine(number1 * number2);
+    }
+    else if (Operator == "/")
+    {
+        Console.WriteLine("Мы разделили числа");
+        Console.WriteLine(number1 / number2);
+    }
+}
+
